200a6ce86ff04f06807f8a7ba74ea4da9db0ee74
Mohamed Mostafa <mohamedm9595@gmail.com>
2024-01-10 10:13:25 +0200

    implement PodcastPlaylistRepositoryImpl in data layer

---
+++ app/src/main/java/com/podcastapp/features/playlist/data/repository/PodcastPlaylistRepositoryImpl.kt
@@ -0,0 +1,23 @@
+package com.podcastapp.features.playlist.data.repository
+
+import com.podcastapp.features.playlist.data.source.map
+import com.podcastapp.features.playlist.data.source.remote.PodcastRemoteDataSource
+import com.podcastapp.features.playlist.domain.PlayList
+import com.podcastapp.features.playlist.domain.PodcastPlaylistRepository
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flatMapConcat
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+class PodcastPlaylistRepositoryImpl @Inject constructor(
+    private val remoteDataSource: PodcastRemoteDataSource
+) : PodcastPlaylistRepository {
+    @OptIn(ExperimentalCoroutinesApi::class)
+    override fun getPlaylist(): Flow<PlayList> {
+        return remoteDataSource.login()
+            .flatMapConcat { remoteDataSource.getPlaylist(it.accessToken) }
+            .map { it.map() }
+    }
+
+}
