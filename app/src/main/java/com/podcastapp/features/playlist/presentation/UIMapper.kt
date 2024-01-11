a5aca8e0b16966b1a8515505ab71f3506c4bed87
Mohamed Mostafa <mohamedm9595@gmail.com>
2024-01-11 09:05:56 +0200

    handle mapping logic in presentation layer

---
+++ app/src/main/java/com/podcastapp/features/playlist/presentation/UIMapper.kt
@@ -0,0 +1,28 @@
+package com.podcastapp.features.playlist.presentation
+
+import com.podcastapp.features.playlist.domain.Episode
+import com.podcastapp.features.playlist.domain.PlayList
+
+fun PlayList.map(): PlaylistUI {
+    return PlaylistUI(
+        info = info,
+        episodes = episodes.map { it.map() }
+    )
+}
+
+fun Episode.map(): EpisodeUI {
+    return EpisodeUI(
+        id = id,
+        state = MediaState.Idle,
+        name = name,
+        desc = desc,
+        image = image,
+        audioLink = audioLink,
+        durationInSeconds = durationInSeconds,
+        podcastName = podcastName,
+        releaseDate = releaseDate,
+        createdAt = createdAt,
+        updatedAt = updatedAt,
+        position = position
+    )
+}
